461aae9afd0f1cf75978bde924bc753d9c510ac1
Mateo <mateog91@gmail.com>
2022-04-28 11:12:52 -0500

    UPD: drop table if exists so it can refresh with new column 'country'

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -8,6 +8,7 @@
 --     If the table already exists, your script should not fail
 --     Your script can be executed on any databas
 
+DROP TABLE IF EXISTS users;
 CREATE TABLE IF NOT EXISTS users (
 	id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 	email VARCHAR(255) NOT NULL UNIQUE,
