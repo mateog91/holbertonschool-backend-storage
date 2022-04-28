cb22626606605532b891d213b17f478b42c67d9a
Mateo <mateog91@gmail.com>
2022-04-28 14:44:57 -0500

    ADD: FILES TASK 3

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -6,5 +6,6 @@
 --     Your script can be executed on any database
 
 -- SELECT band_name, DATEDIFF(split, formed) AS lifespan FROM metal_bands;
-SELECT band_name, IF(split IS NULL, 2020, split) - formed AS lifespan FROM metal_bands ORDER BY lifespan DESC;
+-- SELECT band_name, IF(split IS NULL, 2020, split) - formed AS lifespan FROM metal_bands ORDER BY lifespan DESC;
 -- SELECT ISNULL(split, YEAR('2020')) LIKE '%' FROM metal_bands;
+SELECT band_name, ISNULL(split, 2020) - formed AS lifespan FROM metal_bands ORDER BY lifespan DESC;
