46fe85942fb5548bcb7a392cfbcfc59f80a80867
Mateo <mateog91@gmail.com>
2022-05-03 10:56:07 -0500

    UPD: removed main from file

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -15,10 +15,3 @@ def list_all(mongo_collection):
         Return an empty list if no document in the collection
     """
     return mongo_collection.find()
-
-if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    school_collection = client.my_db.school
-    schools = list_all(school_collection)
-    for school in schools:
-        print("[{}] {}".format(school.get('_id'), school.get('name')))
