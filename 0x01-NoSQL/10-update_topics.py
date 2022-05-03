522aa7c2eb7f8ad8fd2847bf86e95db266b9ea1b
Mateo <mateog91@gmail.com>
2022-05-03 11:49:40 -0500

    FIX: used update_many

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -13,4 +13,4 @@ def update_topics(mongo_collection, name, topics):
         topics (list of strings) will be the list of topics approached in the
         school
     """
-    mongo_collection.update_one({'name': name}, {"$set": {'topics': topics}})
+    mongo_collection.update_many({'name': name}, {"$set": {'topics': topics}})
