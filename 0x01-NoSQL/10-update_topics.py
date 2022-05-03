30696e59f5a90e107f7a6504acebd5d649c7281f
Mateo <mateog91@gmail.com>
2022-05-03 11:45:37 -0500

    ADD: files for task 10

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Module for task 10"""
+from pymongo import MongoClient
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    changes all topics of a school document based on the name:
+
+    Args:
+        mongo_collection: pymongo collection object
+        name (string) will be the school name to update
+        topics (list of strings) will be the list of topics approached in the
+        school
+    """
+    mongo_collection.update_one({'name': name}, {"$set": {'topics': topics}})
