99723df6b4363bf29939d5431f3d9c51b11d26ec
Mateo <mateog91@gmail.com>
2022-05-04 19:00:25 -0500

    removed parenthesis after uuid4

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -7,11 +7,11 @@ from typing import Union
 
 class Cache():
     """
-    Cache class
+    Cache class that stores data in memory
     """
 
     def __init__(self):
-        """Class constructor"""
+        """Class constructor that initialices an instance"""
         self._redis = redis.Redis()
         self._redis.flushdb
 
@@ -25,6 +25,6 @@ class Cache():
         Returns:
             str: uuid key generated where data was stored
         """
-        key = str(uuid.uuid4())
+        key = str(uuid.uuid4)
         self._redis.set(key, data)
         return key
